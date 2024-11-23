02625d92a2fab9ec2fa8e5a5bf1af58b805fc11e
Sławomir Grzebyk <grzebyk.slawomir@gmail.com>
2024-11-23 12:52:09 +0100

    resolve CORS

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+# Konfiguracja CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Pozwala na żądania ze wszystkich domen. Zmień na listę domen, jeśli chcesz to ograniczyć.
+    allow_credentials=True,
+    allow_methods=["*"],  # Pozwala na wszystkie metody (GET, POST, PUT, DELETE itd.)
+    allow_headers=["*"],  # Pozwala na wszystkie nagłówki
+)
+
 
 @app.get("/")
 def read_root():
