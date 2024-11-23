74f865b3c3b4a1cc3c635f692636194159fc803f
Sławomir Grzebyk <grzebyk.slawomir@gmail.com>
2024-11-23 11:06:44 +0100

    create test route

--- app.py
+++ app.py
@@ -4,12 +4,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get('/test')
+def test_data():
+    return {"value": 'This is just response from Back End'}
